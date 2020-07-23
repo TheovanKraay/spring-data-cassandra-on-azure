09f6aedf121b8ddb172ca59288a4f0748dbfe400
TheovanKraay <thvankra@microsoft.com>
2020-07-23 20:04:09 +0100

    add retry and load balancing policy changes

--- src/main/java/com/microsoft/azure/samples/spring/PetController.java
+++ src/main/java/com/microsoft/azure/samples/spring/PetController.java
@@ -6,41 +6,78 @@
 
 package com.microsoft.azure.samples.spring;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import com.datastax.driver.core.Session;
+import com.datastax.driver.core.utils.UUIDs;
+import com.microsoft.azure.cosmos.cassandra.CosmosLoadBalancingPolicy;
+import com.microsoft.azure.cosmos.cassandra.CosmosRetryPolicy;
+
+import org.springframework.data.cassandra.core.CassandraOperations;
+import org.springframework.data.cassandra.core.CassandraTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Optional;
+import java.io.IOException;
 import java.util.UUID;
-
-import com.datastax.oss.driver.api.core.uuid.Uuids;
 
 @Controller
 @RequestMapping(path = "/pets")
 public class PetController {
-    @Autowired
-    private PetRepository petRepository;
+    private static Configurations config = new Configurations();
+
+    private static int PORT;
+    private static String[] CONTACT_POINTS;
+    static {
+        String value;
+        try {
+            value = config.getProperty("spring.data.cassandra.port");
+            PORT = Short.parseShort(value);
+            value = config.getProperty("spring.data.cassandra.contact-points");
+            CONTACT_POINTS = new String[] { value };
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    };
+
+    public PetController() throws Exception {
+    }
+
+    CassandraUtils utils = new CassandraUtils();
+
+    //set retry policy defaults
+    private static final int FIXED_BACK_OFF_TIME = 5000;
+    private static final int GROWING_BACK_OFF_TIME = 1000;
+    private static final int MAX_RETRY_COUNT = 20;
+    public static final int NUMBER_OF_THREADS = 40;
+    public static final int NUMBER_OF_WRITES_PER_THREAD = 5;
+
+    //set Cosmos Retry Policy
+    CosmosRetryPolicy retryPolicy = new CosmosRetryPolicy(MAX_RETRY_COUNT, FIXED_BACK_OFF_TIME, GROWING_BACK_OFF_TIME);
+
+    //set load balancing policy to prefer reads and writes to West US region
+    CosmosLoadBalancingPolicy loadBalancingPolicy = CosmosLoadBalancingPolicy.builder().withWriteDC("West US").withReadDC("West US").build();
+    
+    //create the Cassandra API session with custom policies loaded
+    Session cassandraSession = utils.getSession(CONTACT_POINTS, PORT, retryPolicy, loadBalancingPolicy);
+
+    //create spring data template with session
+    CassandraOperations template = new CassandraTemplate(cassandraSession);
 
     @PostMapping
     public @ResponseBody String createPet(@RequestBody Pet pet) {
-        pet.setId(Uuids.timeBased());
-        petRepository.save(pet);
+        pet.setId(UUIDs.timeBased());
+        template.insert(pet);
+        System.out.println("Added %s."+pet);
         return String.format("Added %s.", pet);
     }
 
-    @GetMapping
-    public @ResponseBody Iterable<Pet> getAllPets() {
-        return petRepository.findAll();
-    }
-
     @GetMapping("/{id}")
-    public @ResponseBody Optional<Pet> getPet(@PathVariable UUID id) {
-        return petRepository.findById(id);
+    public @ResponseBody Pet getPet(@PathVariable UUID id) {
+        return template.selectOneById(id, Pet.class);
     }
 
     @DeleteMapping("/{id}")
     public @ResponseBody String deletePet(@PathVariable UUID id) {
-        petRepository.deleteById(id);
+        template.deleteById(id, Pet.class);
         return "Deleted " + id;
     }
 }
